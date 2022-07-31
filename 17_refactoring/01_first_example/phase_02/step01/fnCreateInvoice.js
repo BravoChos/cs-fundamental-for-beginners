aaf1b4eab48bec5b781fd5bb4cc13f4cd2d015b6
Song Cho <bravochos@gmail.com>
2022-07-31 20:15:37 +0900

    split the phases

---
+++ 17_refactoring/01_first_example/phase_02/step01/fnCreateInvoice.js
@@ -0,0 +1,88 @@
+import invoices from "./invoices.json" assert { type: "json" };
+import plays from "./plays.json" assert { type: "json" };
+
+function statement(invoice, plays) {
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  // remove invoice argument
+  // return renderPlainText(statementData, invoice, plays);
+  return renderPlainText(statementData, invoice, plays);
+}
+
+function renderPlainText(data, _plays) {
+  let result = `Invoice Detail (customer: ${data.customer})\n`;
+
+  for (let perf of data.performances) {
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+      perf.audience
+    } seat(s))\n`;
+  }
+
+  result += `total amount: $${usd(totalAmount())}\n`;
+  result += `extra credits: ${totalVolumeCredits()} points\n`;
+  return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) {
+      result += Math.floor(aPerformance.audience / 5);
+    }
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (playFor(aPerformance).type) {
+      case "family":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+          break;
+        }
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown jenre: ${playFor(aPerformance).type}`);
+    }
+
+    return result;
+  }
+}
+
+console.log(statement(invoices[0], plays));
