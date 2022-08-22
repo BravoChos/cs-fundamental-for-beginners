933bd061052dddd274c7dec5566457232848f0d9
Song Cho <bravochos@gmail.com>
2022-08-22 17:00:44 +0900

    build execute envirements

--- 17_refactoring/06_basic_refactoring/01_extract_function/step01-original/index.js
+++
@@ -1,8 +0,0 @@
-function printOwing(invoice) {
-  printBanner();
-  let outstanding = calculateOutstanding();
-
-  // print details
-  console.log(`client: ${invoice.customer}`);
-  console.log(`debt: ${outstanding}`);
-}
