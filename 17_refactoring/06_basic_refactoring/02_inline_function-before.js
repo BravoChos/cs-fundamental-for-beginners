4891f27a0e37e951a3f7c8542b4cf5835a4b2c2f
Song Cho <bravochos@gmail.com>
2022-08-29 15:29:12 +0900

    inline function done

---
+++ 17_refactoring/06_basic_refactoring/02_inline_function-before.js
@@ -0,0 +1,20 @@
+// Case 1
+export function rating(driver) {
+  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+}
+
+function moreThanFiveLateDeliveries(dvr) {
+  return dvr.numberOfLateDeliveries > 5;
+}
+
+// Case 2
+function reportLines(customer) {
+  const lines = [];
+  gatherCustomerData(lines, customer);
+  return lines;
+}
+
+function gatherCustomerData(out, customer) {
+  out.push(["name", customer.name]);
+  out.push(["location", customer.location]);
+}
