f0dd2c799c6dd15da391e8027e7bb999085d21ba
Song Cho <bravochos@gmail.com>
2022-09-16 20:37:59 +0900

    client 2 example done

---
+++ 17_refactoring/06_basic_refactoring/09_combine_func_into_class-client2-before.js
@@ -0,0 +1,24 @@
+const reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
+
+export function acquireReading() {
+  return reading;
+}
+
+export function baseRate(month, year) {
+  if (year === 2017 && month === 5) return 0.1;
+  return 0.2;
+}
+
+// Client 1
+// const readingClient1 = acquireReading();
+// console.log(readingClient1.baseCharge);
+
+// Client 2
+const aReading = acquireReading();
+const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
+
+function taxThreshold(year) {
+  return 0.1;
+}
+
+export const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
