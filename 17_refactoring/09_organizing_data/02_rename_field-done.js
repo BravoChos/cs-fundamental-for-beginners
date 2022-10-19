862dbe4d04be134dc341c9e8cd1ae6ddef6cc9aa
Song Cho <bravochos@gmail.com>
2022-10-19 21:56:55 +0900

    rename field example done

---
+++ 17_refactoring/09_organizing_data/02_rename_field-done.js
@@ -0,0 +1,22 @@
+class Organization {
+  constructor(data) {
+    this._name = data.name;
+    this._country = data.country;
+  }
+  get title() {
+    return this._name;
+  }
+  set title(value) {
+    this._name = value;
+  }
+  get country() {
+    return this._country;
+  }
+  set country(value) {
+    this._country = value;
+  }
+}
+const organization = new Organization({
+  name: "Humanscape",
+  country: "Korea",
+});
