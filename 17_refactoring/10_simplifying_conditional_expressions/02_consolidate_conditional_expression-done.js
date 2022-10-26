0dc17bb973356bf063c1053401d33e1aac9b4959
Song Cho <bravochos@gmail.com>
2022-10-26 18:51:24 +0900

    consolidate conditional expressions done

---
+++ 17_refactoring/10_simplifying_conditional_expressions/02_consolidate_conditional_expression-done.js
@@ -0,0 +1,15 @@
+function disabilityAmount(employee) {
+    if (isNotEligibleForDisability(employee)) {
+      return 0;
+    }
+    return 1;
+  }
+  
+  function isNotEligibleForDisability(employee) {
+    return (
+      employee.seniority < 2 ||
+      employee.monthsDisabled > 12 ||
+      employee.isPartTime
+    );
+  }
+  
