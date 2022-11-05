254e4cbb6f887ea95132f7b9da10832d7e9446ee
Song Cho <bravochos@gmail.com>
2022-11-06 04:00:57 +0900

    remove flag argument done

---
+++ 17_refactoring/11_API_refactoring/03_remove_flag_argument-done.js
@@ -0,0 +1,20 @@
+// example 1
+function setWidth(value) {
+    this._width = value;
+  }
+  
+  function setHeight(value) {
+    this._height = value;
+  }
+  
+  // example 2
+  class Concert {
+    regularBook(customer) {}
+    premiumBook(customer) {}
+    #book(customer, isPremium) {}
+  }
+  
+  // example 3
+  function swtichOn();
+  function swtichOff();
+  
