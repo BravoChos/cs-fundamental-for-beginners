d136da2dde77b2313732f1a138809029af99246a
Song Cho <bravochos@gmail.com>
2022-11-18 07:43:12 +0900

    pull up method done

---
+++ 17_refactoring/12_dealing_with_inheritance/01_pull_up_method-done.js
@@ -0,0 +1,18 @@
+// example 1
+class Employee {
+  get name() {}
+}
+
+class Salesperson extends Employee {}
+
+class Engineer extends Employee {}
+
+// example 2
+class Party {
+  get annualCost() {
+    return this.monthlyCost * 12;
+  }
+}
+
+class Department extends Party {}
+class Employee extends Party {}
